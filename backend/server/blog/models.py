cabf886dac24cd7b76c9aea84e445d38b7a0d451
akhand6886 <aksingh6886@GMAIL.COM>
2025-01-23 01:07:36 +0530

    Models for blog || admin panel

--- backend/server/blog/models.py
+++ backend/server/blog/models.py
@@ -1,11 +1,11 @@
+# blog/models.py
 from django.db import models
-from django.contrib.auth.models import User
 
-class Post(models.Model):
+class Blog(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    category = models.CharField(max_length=100, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)  # optional if you want user integration
 
     def __str__(self):
         return self.title
