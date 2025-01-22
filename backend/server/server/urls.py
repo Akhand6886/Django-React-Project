323d2c5ef4af25efa4663defd3840aa0ed65d84a
akhand6886 <aksingh6886@GMAIL.COM>
2025-01-23 01:09:50 +0530

    ex

--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -17,6 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+from rest_framework.routers import DefaultRouter
+from blog.views import BlogViewSet
+router = DefaultRouter()
+router.register(r'blogs', BlogViewSet, basename='blogs')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
