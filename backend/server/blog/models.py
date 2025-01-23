a61d4ee3eac3c85274067734d3bde399d24c698b
akhand6886 <aksingh6886@GMAIL.COM>
2025-01-23 19:16:08 +0530

    database error fixed

--- backend/server/blog/models.py
+++ backend/server/blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    category = models.CharField(max_length=100)
+    category = models.CharField(max_length=100, default="General")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
