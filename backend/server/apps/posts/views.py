3f51f3469bdeefbb0f41227a9a7628fd9b4989dd
akhand6886 <aksingh6886@GMAIL.COM>
2025-01-27 13:48:58 +0530

    added other backed codes

--- backend/server/apps/posts/views.py
+++ backend/server/apps/posts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    http_method_names = ['get', 'post', 'put', 'patch', 'delete']
--- backend/server/apps/users/views.py
+++ backend/server/apps/users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]  # Example: require authentication
+    http_method_names = ['get', 'post', 'put', 'patch', 'delete']
