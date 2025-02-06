24ba9259bf9011952f0b6e25e709b34ab5a009e9
akhand6886 <aksingh6886@GMAIL.COM>
2025-02-06 17:58:24 +0530

    added code url for jwt auth endpoints

--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
-from django.urls import path, include  
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/users/', include('apps.users.urls')),
     path('api/posts/', include('apps.posts.urls')),
     path('api/categories/', include('apps.categories.urls')),
+
+# JWT authentication endpoints
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
